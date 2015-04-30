c78c195e8e740011455e64d94e1e2d7f7903b650
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-04-30 15:44:58 -0400

    Split into "deinprogramm" and "deinprogramm-signature" pkgs.
    This enables "htdp-lib" to depend only on the signature code.

---
+++ deinprogramm-signature/info.rkt
@@ -0,0 +1,15 @@
+#lang info
+(define collection 'multi)
+
+(define deps '("base"
+               "compatibility-lib"
+               "drracket-plugin-lib"
+               "gui-lib"
+               "htdp-lib"
+               "scheme-lib"
+               "srfi-lib"
+               "string-constants-lib"))
+
+(define pkg-desc "Signature support for teaching languages for _Die Macht der Abstraktion_")
+
+(define pkg-authors '(sperber))
