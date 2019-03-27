6376c012cbc7fa9a44c08f8658c0f92672cb6dd9
Mike Sperber <sperber@deinprogramm.de>
2019-03-27 16:44:16 +0100

    Zap empty-list from SdP beginner

--- deinprogramm/deinprogramm/sdp/beginner.rkt
+++ deinprogramm/deinprogramm/sdp/beginner.rkt
@@ -10,13 +10,13 @@
 	 check-expect check-within check-error check-member-of check-range check-satisfied
 	 check-property for-all ==> expect expect-within expect-member-of expect-range
 	 signature contract : define-contract -> mixed one-of predicate combined
-	 number real rational integer natural boolean true false string empty-list any property)
+	 number real rational integer natural boolean true false string any property)
 (provide-and-document
  procedures
  (all-from-except beginner: deinprogramm/sdp/private/primitives procedures
 		  set! eq? equal?
 		  quote
-		  make-pair pair? cons? first rest
+		  empty make-pair pair? cons? first rest
 		  length map for-each reverse append list list-ref fold
 		  symbol? symbol=? string->symbol symbol->string
 		  apply))
