fd1ae7f1b1fee98c52fe66164a7483046158fb45
Mike Sperber <sperber@deinprogramm.de>
2026-01-19 17:25:37 +0100

    Declare DeinProgramm language-family

--- deinprogramm-doc/deinprogramm/scribblings/info.rkt
+++ deinprogramm-doc/deinprogramm/scribblings/info.rkt
@@ -1,3 +1,7 @@
 #lang info
 
 (define scribblings '(("deinprogramm.scrbl" (multi-page) (teaching -21))))
+
+(define language-family (list (hash 'family "DeinProgramm"
+                                    'doc '(lib "deinprogramm/scribblings/deinprogramm.scrbl")
+                                    'order -100)))
