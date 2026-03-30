f399363885269a4c956869ef697605980820d63d
Mike Sperber <sperber@deinprogramm.de>
2026-03-30 15:39:45 +0200

    Tests for define-record and define-singleton signature declarations

--- deinprogramm-test/deinprogramm/sdp/tests/test-engine-program-test.rkt
+++ deinprogramm-test/deinprogramm/sdp/tests/test-engine-program-test.rkt
@@ -85,3 +85,27 @@
                unequal?
                unequal-actual 1
                unequal-expected 2)
+
+(check-success #<<--
+#lang deinprogramm/sdp
+
+(define-record foo
+  make-foo
+  foo?
+  (foo-bar number))
+
+(: make-foo (number -> foo))
+(: foo? (any -> boolean))
+(: foo-bar (foo -> number))
+--
+               )
+
+(check-success #<<--
+#lang deinprogramm/sdp
+(define-singleton empty-list empty empty?)
+
+(: empty empty-list)
+
+(: empty? (any -> boolean))
+--
+               )
